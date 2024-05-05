fee9ff12b1648b8289765e5ca855d0ebad2f9ef4
teradonburi <daikiterai@gmail.com>
2024-05-05 22:42:26 +0900

    dist

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	root: ".",
+	plugins: [],
+	build: {
+		// distフォルダに出力
+		outDir: resolve(__dirname, "dist"),
+		// 存在しないときはフォルダを作成する
+		emptyOutDir: true,
+		rollupOptions: {
+			// entry pointがあるindex.htmlのパス
+			input: {
+				"": resolve(__dirname, "index.html"),
+			},
+			// bundle.jsを差し替えする
+			output: {
+				entryFileNames: 'assets/bundle.js',
+			},
+		},
+	},
+});
