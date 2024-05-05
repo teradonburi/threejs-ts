d4ffbd906c2712f55222c2335af0ce8748666e39
teradonburi <daikiterai@gmail.com>
2024-05-05 23:20:58 +0900

    docs

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,13 @@ export default defineConfig({
 	plugins: [],
 	build: {
 		// docsフォルダに出力
-		outDir: resolve(__dirname, "docs"),
+		outDir: resolve(__dirname, "dist"),
 		// 存在しないときはフォルダを作成する
 		emptyOutDir: true,
 		rollupOptions: {
 			// entry pointがあるindex.htmlのパス
 			input: {
-				"": resolve(__dirname, "index.html"),
+				"": resolve(__dirname, "src/index.html"),
 			},
 			// bundle.jsを差し替えする
 			output: {
